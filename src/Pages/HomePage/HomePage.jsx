992a31a250fc4237e4a064ee790fea197299c85c
Roy Joseph <royjoseph.mec@gmail.com>
2023-08-25 21:41:42 +0530

    Almost finished the home page.

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -6,6 +6,7 @@ import Carousel from "../../components/Carousel/Carousel"
 import Ads from "../../components/Ads/Ads"
 import { ToastContainer } from "react-toastify"
 import Collaborations from "../../components/Collaborations/Collaborations"
+import { Link } from "react-router-dom"
 
 function HomePage() {
 
@@ -18,6 +19,15 @@ function HomePage() {
                     <h2>FREE RETURNS ON ALL ORDERS</h2>
                 </div>
                 <Ads/>
+                <div className="Community">
+                    <img src="./images/Community.png" alt="Our Community" />
+                    <span>SINCE 2024</span>
+                    <p>HELPING OUR COMMUNITY EXPLORE THEIR INNER STREET ARTIST</p>
+                    <span>Our aim is to build the biggest self-sustaining community by empowering vari.0s artists and athletes.</span>
+                    <Link to="/shop">
+                        Get Your Drip!
+                    </Link>
+                </div>
                 <Collaborations/>
             </div>
             <ToastContainer />
