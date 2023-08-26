d52bec72694abc52f2177f3ff997d6c175c05db5
Roy Joseph <royjoseph.mec@gmail.com>
2023-08-26 21:05:43 +0530

    Added a ScrollToTop component
    It makes the window to scroll to top when the path chnages.

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import userReducer from './slices/user.jsx'
+import ScrollToTop from './ScrollToTop.jsx'
 
 const store = configureStore({
   reducer: {
@@ -16,6 +17,7 @@ const store = configureStore({
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Router>
       <Provider store={store}>
+        <ScrollToTop/>
         <Routes>
           <Route path='*' element={<App/>}/>
         </Routes>
