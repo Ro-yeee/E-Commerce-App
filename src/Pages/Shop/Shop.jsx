73956c2e5fea3cfbea689789a5e3a0b68141c6c4
Roy Joseph <royjoseph.mec@gmail.com>
2023-08-27 14:41:06 +0530

    Pictures of products are temporaly displayed in shop.

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,11 +1,23 @@
+import { useSelector } from "react-redux"
 import SideNav from "../../components/SideNav/SideNav"
 import "./Shop.css"
 
 function Shop() {
+
+  const products = useSelector(state => state.products)
+
   return (
     <div className="Shop">
         <SideNav/>
-        <h1>SHOP</h1>
+        <div className="ShopContainer">
+            {
+                products.all.map(ele =>(
+                    <div className="picccs">
+                       <img src={ele.picture} alt=""/>
+                    </div>
+                ))
+            }
+        </div>
     </div>
   )
 }
