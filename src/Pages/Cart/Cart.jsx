23dbeeed9f85bf036116ca4f839d5cb27427abe5
Roy Joseph <royjoseph.mec@gmail.com>
2023-08-30 21:08:13 +0530

    Added empty cart meesage when cart is empty.

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -3,6 +3,7 @@ import SideNav from "../../components/SideNav/SideNav"
 import "./Cart.css"
 import ProductListView from "../../components/ProductListView/ProductListView"
 import CartProductCard from "../../components/CartProductCrad/CartProductCard"
+import { Link } from "react-router-dom"
 
 function Cart() {
 
@@ -14,7 +15,16 @@ function Cart() {
     <div className="Cart">
         <SideNav/>
         <div className="CartContainer">
-            <h2 className="cartHeading">{user.name.split(" ")[0]}'s Cart ({total_qty} Items)</h2>
+            <h2 className="cartHeading">{user.name.split(" ")[0]}'s Cart ( {total_qty} Items )</h2>
+            {
+                cart.length < 1 && 
+                    <div className="emptyMessage">
+                        <h2>Your Cart Is Empty</h2>
+                        <Link to="/shop">
+                            Shop Now
+                        </Link>
+                    </div>
+            }
             <CartProductCard products={cart}/>
         </div>
     </div>
