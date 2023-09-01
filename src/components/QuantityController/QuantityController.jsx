a2f7d23f9e00484dc5781cad04f5653a2721184b
Roy Joseph <royjoseph.mec@gmail.com>
2023-09-01 18:24:03 +0530

    Rectified Error in Cart DB update.
    There was an issue of db getting added by the saem item again instead of
    updating it when user chnages the quantity quickly, this was resolved by adding,
    a loading state and making it false and true to make the buttons to be enabled
    or disabled.

--- src/components/QuantityController/QuantityController.jsx
+++ src/components/QuantityController/QuantityController.jsx
@@ -5,16 +5,19 @@ import { useDispatch, useSelector } from "react-redux"
 import { changeQuantity } from "../../slices/cart"
 import { arrayRemove, arrayUnion, doc, updateDoc } from "firebase/firestore"
 import { db } from "../../FirebaseConfig"
+import { useState } from "react"
 
 function QuantityController({product}) {
 
     const dispatch = useDispatch()
     const user = useSelector(state => state.user)
     const {id,name,qty,price,picture} = product
+    const [loading,setLoading] = useState(false)
 
     const handleQtyControl = (type)=>{
         if(type === "decrease" && qty === 1 ) return
         else if(type === "decrease"){
+            setLoading(true)
             updateDoc(doc(db,"users",`${user.id}`),{
                 cart : arrayRemove({
                     id,
@@ -37,9 +40,11 @@ function QuantityController({product}) {
             })
             .then(()=>{
                 dispatch(changeQuantity({id,type}))
+                setLoading(false)
             })
             .catch(error => console.log(error))
         }else{
+            setLoading(true)
             updateDoc(doc(db,"users",`${user.id}`),{
                 cart : arrayRemove({
                     id,
@@ -62,6 +67,7 @@ function QuantityController({product}) {
             })
             .then(()=>{
                 dispatch(changeQuantity({id,type}))
+                setLoading(false)
             })
             .catch(error => console.log(error))
         }
@@ -69,11 +75,11 @@ function QuantityController({product}) {
 
   return (
     <div className="quantityControl">
-        <button onClick={()=>handleQtyControl("decrease")}>
+        <button disabled={loading} onClick={()=>handleQtyControl("decrease")}>
             <FontAwesomeIcon className="quantityControlIcons" size="xl" icon={faCircleMinus}/>
         </button>
         <output>{qty}</output>
-        <button onClick={()=>handleQtyControl("increase")}>
+        <button disabled={loading} onClick={()=>handleQtyControl("increase")}>
             <FontAwesomeIcon className="quantityControlIcons" size="xl" icon={faCirclePlus}/>
         </button>
     </div>
